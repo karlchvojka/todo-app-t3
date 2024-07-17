839f4329109b0f10de59a168038e1dd11c0ade2c
Karl Chvojka <karl.chvojka@protonmail.com>
2024-07-17 14:14:11 -0400

    INIT: Initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+import { LatestPost } from "~/app/_components/post";
+import { getServerAuthSession } from "~/server/auth";
+import { api, HydrateClient } from "~/trpc/server";
+
+export default async function Home() {
+  const hello = await api.post.hello({ text: "from tRPC" });
+  const session = await getServerAuthSession();
+
+  void api.post.getLatest.prefetch();
+
+  return (
+    <HydrateClient>
+      <main>
+        <h1>Create T3 Todo App</h1>
+      </main>
+    </HydrateClient>
+  );
+}
