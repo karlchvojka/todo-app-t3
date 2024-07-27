ca61ee50b6ca0c0c86658ec8d8d0bdaf1615f29a
Karl Chvojka <karl.chvojka@protonmail.com>
2024-07-27 00:49:08 -0400

    IMP: Logo into feature
    Added logo and cleaned up alignment of header

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,58 +1,59 @@
-
 import NavLink from "~/app/_components/nav-link";
+import "~/styles/globalicons.css";
 
 /* TYPES */
 interface NavItemsProps {
-  label: string,
-  href: string
+  label: string;
+  href: string;
 }
 
 export default function Header() {
   const navItems: Array<{
-    label: string, href: string
+    label: string;
+    href: string;
   }> = [
-      {
-        label: 'Home',
-        href: '#' 
-      },
-      {
-        label: 'Todos',
-        href: '#' 
-      },
-      {
-        label: 'Profile',
-        href: '#' 
-      },
-      {
-        label: 'Login',
-        href: '#' 
-      },
-      {
-        label: 'Logout',
-        href: '#' 
-      },
-    ];
+    {
+      label: "Todos",
+      href: "#",
+    },
+    {
+      label: "Profile",
+      href: "#",
+    },
+    {
+      label: "Login",
+      href: "#",
+    },
+    {
+      label: "Logout",
+      href: "#",
+    },
+  ];
 
   return (
-    <div id="headerWrap" className="flex flex-col md:flex-row shadow-lg bg-slate-700">
-        <div className="p-4 md:w-1/2">
-          <h1 className="text-center">TodoApp</h1>
-        </div>
-        <div id="navWrap" className="p-4 md:w-3/4">
-          <ul className="w-full flex flex-col gap-4 md:flex-row">
-            {
-              navItems.map((item: NavItemsProps, index) => {
-                return (
-                  <NavLink 
-                    key={item.label + index} 
-                    label={item.label} 
-                    href={item.href} 
-                  />
-                )
-              })
-            }
-          </ul>
-        </div>
+    <div
+      id="headerWrap"
+      className="flex flex-col justify-between bg-slate-700 p-4 shadow-lg md:flex-row"
+    >
+      <span className="material-symbols-outlined self-center !text-6xl">
+        check_box
+      </span>
+      <div className="self-center">
+        <h1 className="text-center">TodoApp</h1>
       </div>
-  )
+      <div id="navWrap" className="flex flex-row p-4">
+        <ul className="flex w-full flex-col gap-4 md:flex-row">
+          {navItems.map((item: NavItemsProps, index) => {
+            return (
+              <NavLink
+                key={item.label + index}
+                label={item.label}
+                href={item.href}
+              />
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
