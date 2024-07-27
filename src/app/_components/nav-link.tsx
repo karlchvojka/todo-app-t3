9a48b9aa14eb2a39cec3f81c22f88e4b92b395b8
Karl Chvojka <karl.chvojka@protonmail.com>
2024-07-27 02:47:04 -0400

    IMP: Navlinks
    Fixed nav link fontsize fixes

--- src/app/_components/nav-link.tsx
+++ src/app/_components/nav-link.tsx
@@ -7,7 +7,7 @@ export default function NavLink({ href, label }: Props) {
   return (
     <li className="w-full">
       <a
-        className="block w-full rounded-md border-0 bg-lime-900 p-4 text-center text-2xl font-bold uppercase text-white shadow-sm hover:border-slate-900 hover:bg-lime-600 hover:text-black"
+        className="block w-full rounded-md border-0 bg-lime-900 p-2 text-center text-xl font-bold uppercase text-white shadow-sm hover:border-slate-900 hover:bg-lime-600 hover:text-black"
         href={href}
       >
         {label}
