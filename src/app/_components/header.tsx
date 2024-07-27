4f586691dc38a5853f944386d1e26f1d21840407
Karl Chvojka <karl.chvojka@protonmail.com>
2024-07-26 21:23:16 -0400

    FEA: Nav Links
    Implement Nav Links into header in map

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,10 +1,58 @@
-"use client";
 
-export function Header() {
+import NavLink from "~/app/_components/nav-link";
+
+/* TYPES */
+interface NavItemsProps {
+  label: string,
+  href: string
+}
+
+export default function Header() {
+  const navItems: Array<{
+    label: string, href: string
+  }> = [
+      {
+        label: 'Home',
+        href: '#' 
+      },
+      {
+        label: 'Todos',
+        href: '#' 
+      },
+      {
+        label: 'Profile',
+        href: '#' 
+      },
+      {
+        label: 'Login',
+        href: '#' 
+      },
+      {
+        label: 'Logout',
+        href: '#' 
+      },
+    ];
 
   return (
-    <div className="headerWrap">
-      <h2>Header Wrap</h2>
-    </div>
+    <div id="headerWrap" className="flex flex-col md:flex-row shadow-lg bg-slate-700">
+        <div className="p-4 md:w-1/2">
+          <h1 className="text-center">TodoApp</h1>
+        </div>
+        <div id="navWrap" className="p-4 md:w-3/4">
+          <ul className="w-full flex flex-col gap-4 md:flex-row">
+            {
+              navItems.map((item: NavItemsProps, index) => {
+                return (
+                  <NavLink 
+                    key={item.label + index} 
+                    label={item.label} 
+                    href={item.href} 
+                  />
+                )
+              })
+            }
+          </ul>
+        </div>
+      </div>
   )
 }
